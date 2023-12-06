a9b5be6d7e8dc77e45f6a89a5c6d214712248d84
TimbirICT <tmiddlebrooks99@gmail.com>
2023-12-05 21:47:25 -0600

    heroku fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const api = require('./routes/api.js');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const fs = require('fs');
 const dbFilePath = path.join(__dirname, 'db', 'db.json');
